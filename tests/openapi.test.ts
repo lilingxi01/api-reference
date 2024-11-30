7dc371e996c814fd0619f4d3e4b2c01770949f6d
Lingxi Li <lilingxi01@gmail.com>
2024-11-30 00:41:12 -0800

    feat: more tests.

--- tests/openapi.test.ts
+++ tests/openapi.test.ts
@@ -232,3 +232,422 @@ describe("GET request transformation", () => {
     expect(expectedSpec).toMatchObject(transformedSpec);
   });
 });
+
+describe("POST request transformation", () => {
+  const exampleSpec: OpenAPIV3_1.Document = {
+    openapi: "3.1.0",
+    info: {
+      title: "Test API",
+      version: "0.1.0",
+    },
+    paths: {
+      "/users": {
+        get: {
+          tags: ["Users"],
+          summary: "List Users",
+          description: "List all users.",
+          operationId: "list_users_users_get",
+          parameters: [
+            {
+              name: "limit",
+              in: "query",
+              description: "Maximum number of users to return",
+              required: false,
+              schema: {
+                type: "integer",
+                description: "Maximum number of users to return",
+                maximum: 50,
+                minimum: 1,
+                default: 50,
+                title: "Limit",
+              },
+            },
+            {
+              name: "cursor",
+              in: "query",
+              description:
+                "Cursor from the previous call to list users, used to retrieve the next set of results",
+              required: false,
+              schema: {
+                type: "string",
+                description:
+                  "Cursor from the previous call to list users, used to retrieve the next set of results",
+                title: "Cursor",
+              },
+            },
+          ],
+          responses: {
+            "200": {
+              description: "Successful Response",
+              content: {
+                "application/json": {
+                  schema: {
+                    $ref: "#/components/schemas/GetUsersResponse",
+                  },
+                },
+              },
+            },
+          },
+        },
+        post: {
+          tags: ["Users"],
+          summary: "Create User",
+          operationId: "create_user_users_post",
+          requestBody: {
+            content: {
+              "multipart/form-data": {
+                schema: {
+                  $ref: "#/components/schemas/Body_create_user_users_post",
+                },
+              },
+            },
+            required: true,
+          },
+          responses: {
+            "200": {
+              description: "Successful Response",
+              content: {
+                "application/json": {
+                  schema: {
+                    $ref: "#/components/schemas/CreateUserResponse",
+                  },
+                },
+              },
+            },
+            "401": {
+              description: "Unauthorized",
+            },
+          },
+        },
+      },
+    },
+    components: {
+      schemas: {
+        GetUsersResponse: {
+          type: "array",
+          items: {
+            $ref: "#/components/schemas/GetUserResponse",
+          },
+        },
+        GetUserResponse: {
+          type: "object",
+          properties: {
+            id: {
+              type: "string",
+              format: "uuid",
+              description: "User ID",
+            },
+            name: {
+              type: "string",
+              description: "User's name",
+            },
+          },
+          required: ["id", "name"],
+          title: "GetUserResponse",
+        },
+        CreateUserResponse: {
+          type: "object",
+          properties: {
+            id: { type: "string", format: "uuid", description: "User ID" },
+            name: { type: "string", description: "User's name" },
+          },
+        },
+        Body_create_user_users_post: {
+          type: "object",
+          properties: {
+            name: { type: "string", description: "User's name" },
+          },
+          required: ["name"],
+          title: "Body_create_user_users_post",
+        },
+      },
+    },
+  };
+
+  const expectedSpec: APIReferenceCore = {
+    routes: [
+      {
+        id: "list_users_users_get",
+        path: "/users",
+        method: RouteMethod.GET,
+        title: "List Users",
+        tags: [["Users"]],
+        queryParams: {
+          limit: {
+            description: "Maximum number of users to return",
+            title: "Limit",
+            type: "integer",
+            maximum: 50,
+            minimum: 1,
+            default: 50,
+          },
+          cursor: {
+            description:
+              "Cursor from the previous call to list users, used to retrieve the next set of results",
+            title: "Cursor",
+            type: "string",
+          },
+        },
+        responses: {
+          200: {
+            contentType: ContentType.JSON,
+            description: "Successful Response",
+            body: {
+              type: "array",
+              items: {
+                type: "object",
+                properties: {
+                  id: {
+                    type: "string",
+                    format: "uuid",
+                    description: "User ID",
+                  },
+                  name: {
+                    type: "string",
+                    description: "User's name",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      {
+        id: "create_user_users_post",
+        path: "/users",
+        method: RouteMethod.POST,
+        title: "Create User",
+        tags: [["Users"]],
+        contentType: ContentType.MULTIPART,
+        body: {
+          type: "object",
+          properties: {
+            name: { type: "string", description: "User's name" },
+          },
+        },
+        responses: {
+          200: {
+            contentType: ContentType.JSON,
+            description: "Successful Response",
+            body: {
+              type: "object",
+              properties: {
+                id: { type: "string", format: "uuid", description: "User ID" },
+                name: { type: "string", description: "User's name" },
+              },
+            },
+          },
+          401: {
+            description: "Unauthorized",
+          },
+        },
+      },
+    ],
+  };
+
+  const transformedSpec = transformOpenAPISpec(exampleSpec);
+
+  test("Body", () => {
+    for (const [index, route] of transformedSpec.routes.entries()) {
+      expect(route.body).toEqual(expectedSpec.routes[index].body);
+    }
+  });
+
+  test("Total equality", () => {
+    expect(expectedSpec).toMatchObject(transformedSpec);
+  });
+});
+
+describe("Special types transformation", () => {
+  const exampleSpec: OpenAPIV3_1.Document = {
+    openapi: "3.1.0",
+    info: {
+      title: "Test API",
+      version: "0.1.0",
+    },
+    paths: {
+      "/users": {
+        post: {
+          tags: ["Users"],
+          summary: "Create User",
+          operationId: "create_user_users_post",
+          requestBody: {
+            content: {
+              "multipart/form-data": {
+                schema: {
+                  $ref: "#/components/schemas/Body_create_user_users_post",
+                },
+              },
+            },
+            required: true,
+          },
+          responses: {
+            "200": {
+              description: "Successful Response",
+              content: {
+                "application/json": {
+                  schema: {
+                    $ref: "#/components/schemas/CreateUserResponse",
+                  },
+                },
+              },
+            },
+            "401": {
+              description: "Unauthorized",
+            },
+          },
+        },
+      },
+    },
+    components: {
+      schemas: {
+        CreateUserResponse: {
+          type: "object",
+          properties: {
+            id: { type: "string", format: "uuid", description: "User ID" },
+            name: { type: "string", description: "User's name" },
+          },
+        },
+        Body_create_user_users_post: {
+          type: "object",
+          properties: {
+            name: { type: "string", description: "User's name" },
+            role: {
+              $ref: "#/components/schemas/Role",
+            },
+            overriddenRole: {
+              $ref: "#/components/schemas/Role",
+              enum: ["user"],
+              description: "Overridden role",
+            },
+            numberMinMax: {
+              type: "number",
+              description: "Number with min, max, and default",
+              minimum: 1,
+              maximum: 10,
+              default: 5,
+            },
+            integerMinMax: {
+              type: "integer",
+              description: "Integer with min, max, and default",
+              minimum: 1,
+              maximum: 10,
+              default: 5,
+            },
+          },
+          required: ["name"],
+          title: "Body_create_user_users_post",
+        },
+        Role: {
+          type: "string",
+          enum: ["superadmin", "admin", "user"],
+          description: "User's role",
+        },
+      },
+    },
+  };
+
+  const expectedSpec: APIReferenceCore = {
+    routes: [
+      {
+        id: "create_user_users_post",
+        path: "/users",
+        method: RouteMethod.POST,
+        title: "Create User",
+        tags: [["Users"]],
+        contentType: ContentType.MULTIPART,
+        body: {
+          type: "object",
+          properties: {
+            name: { type: "string", description: "User's name" },
+            role: {
+              type: "string",
+              enum: ["superadmin", "admin", "user"],
+              description: "User's role",
+            },
+            overriddenRole: {
+              type: "string",
+              enum: ["user"],
+              description: "Overridden role",
+            },
+            numberMinMax: {
+              type: "number",
+              description: "Number with min, max, and default",
+              minimum: 1,
+              maximum: 10,
+              default: 5,
+            },
+            integerMinMax: {
+              type: "integer",
+              description: "Integer with min, max, and default",
+              minimum: 1,
+              maximum: 10,
+              default: 5,
+            },
+          },
+        },
+        responses: {
+          200: {
+            contentType: ContentType.JSON,
+            description: "Successful Response",
+            body: {
+              type: "object",
+              properties: {
+                id: { type: "string", format: "uuid", description: "User ID" },
+                name: { type: "string", description: "User's name" },
+              },
+            },
+          },
+          401: {
+            description: "Unauthorized",
+          },
+        },
+      },
+    ],
+  };
+
+  const transformedSpec = transformOpenAPISpec(exampleSpec);
+
+  test("Enum works", () => {
+    expect(transformedSpec.routes[0].body).toBeDefined();
+    expect(transformedSpec.routes[0].body?.type).toEqual("object");
+    if (
+      transformedSpec.routes[0].body?.type === "object" &&
+      transformedSpec.routes[0].body.properties.role.type === "string"
+    ) {
+      expect(transformedSpec.routes[0].body.properties.role.enum).toEqual([
+        "superadmin",
+        "admin",
+        "user",
+      ]);
+      console.log(transformedSpec.routes[0].body.properties);
+      expect(
+        transformedSpec.routes[0].body.properties.role.description,
+      ).toEqual("User's role");
+    } else {
+      expect(false).toBe(true);
+    }
+  });
+
+  test("Enum override works", () => {
+    expect(transformedSpec.routes[0].body).toBeDefined();
+    expect(transformedSpec.routes[0].body?.type).toEqual("object");
+    if (
+      transformedSpec.routes[0].body?.type === "object" &&
+      transformedSpec.routes[0].body.properties.overriddenRole.type === "string"
+    ) {
+      expect(
+        transformedSpec.routes[0].body.properties.overriddenRole.enum,
+      ).toEqual(["user"]);
+      expect(
+        transformedSpec.routes[0].body.properties.overriddenRole.description,
+      ).toEqual("Overridden role");
+    } else {
+      expect(false).toBe(true);
+    }
+  });
+
+  test("Total equality", () => {
+    expect(expectedSpec).toMatchObject(transformedSpec);
+  });
+});
